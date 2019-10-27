19fb8c26bf580370bd6ef01e8ad7fd1987ff6937
Woo <JWoo@eleadcrm.com>
2019-10-27 17:51:38 -0500

    express

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3005;
 
-app.use(express.static('_book'))
+app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
